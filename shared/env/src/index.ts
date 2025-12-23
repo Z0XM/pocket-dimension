ca88d83d30b75ef27cde3c6bd3c4fb4bf100e651
z0xm <mukulz2020@gmail.com>
2025-12-24 02:16:48 +0530

    fixes

--- shared/env/src/index.ts
+++ shared/env/src/index.ts
@@ -1,7 +1,4 @@
-import { config } from "dotenv";
 import { z } from "zod";
-
-config({ path: ".env" });
 
 export const baseEnvSchema = z.object({
   NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
@@ -11,8 +8,7 @@ export const baseEnvSchema = z.object({
  * Validate environment variables
  * @returns Validated environment variables
  */
-export function validateEnv<T extends z.ZodRawShape>(
-  appEnvSchema: T
-): z.infer<z.ZodObject<T & typeof baseEnvSchema>> {
-  return baseEnvSchema.extend(appEnvSchema).parse(process.env);
+export function validateEnv<T extends z.ZodRawShape>(appEnvSchema: z.ZodObject<T>) {
+  const schema = baseEnvSchema.extend(appEnvSchema);
+  return schema.parse(Bun.env) as z.infer<typeof baseEnvSchema> & z.infer<z.ZodObject<T>>;
 }
