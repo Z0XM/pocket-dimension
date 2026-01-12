8a68534817f4a7b402fc0c7d4551056a5e97eaa9
z0xm <mukulz2020@gmail.com>
2026-01-12 21:09:52 +0530

    lazy env

--- shared/db/src/lib/env.ts
+++ shared/db/src/lib/env.ts
@@ -5,4 +5,13 @@ const schema = z.object({
   DATABASE_URL: z.url(),
 });
 
-export const env = validateEnv("db", schema, Bun.env);
+let _env: z.infer<typeof schema> | null = null;
+
+export const env = new Proxy({} as z.infer<typeof schema>, {
+  get(_, prop: string) {
+    if (!_env) {
+      _env = validateEnv("db", schema, Bun.env);
+    }
+    return _env[prop as keyof typeof _env];
+  },
+});
