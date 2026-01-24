f8ab9b84bf687152342656004767cdb5465af401
z0xm <mukulz2020@gmail.com>
2026-01-25 01:58:08 +0530

    added mobile only features

--- scripts/src/watchlist/ratings.ts
+++ scripts/src/watchlist/ratings.ts
@@ -16,13 +16,9 @@ const existingWatchItemsMap = new Map(existingWatchItems.map((item) => [item.tit
 
 const watchItemRatings: (typeof schema.watchItemRatings.$inferInsert)[] = [];
 
-const usernames = ["z0xm", "lordsparos", "legend", "DeadPoOl", "lights"];
+const usernames = ["deadpool"];
 const userColumnMap: Record<string, string> = {
-  z0xm: "z0xm",
-  lordsparos: "imtiyaz",
-  legend: "legend",
-  DeadPoOl: "deadpool",
-  lights: "lights",
+  deadpool: "deadpool",
 };
 
 const users = await db.query.user.findMany({
